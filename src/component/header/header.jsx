219e8532bad3c3e06068f1e3037d95d08ef74da7
unknown <aliopdandan@gmail.com>
2025-01-14 15:57:08 +0100

    f

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export function Header(){
-    return <h1 className="text-2xl">hello world</h1>
+    return (
+        <div className="flex justify-around">
+            <div className="flex">
+                <h1 className="text-2xl">Shortly</h1>
+                <ul>
+                    <li>Features</li>
+                    <li>Pricing</li>
+                    <li>Resources</li>
+                </ul>
+            </div>
+            <div>
+                <div>Login</div>
+                <button>Sign Up</button>
+            </div>
+        </div>
+    )
 }
